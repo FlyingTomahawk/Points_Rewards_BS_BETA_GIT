16bdf47e4233c16fac5efeb59d182f0d743c74d7
raiwa <info@sarplataygemas.com>
2017-09-10 13:15:00 +0200

    modified core files, original modifications

---
+++ modified_core_files/catalog/includes/languages/english/create_account_success.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Create an Account');
+define('NAVBAR_TITLE_2', 'Success');
+define('HEADING_TITLE', 'Your Account Has Been Created!');
+define('TEXT_ACCOUNT_CREATED', 'Congratulations! Your new account has been successfully created! You can now take advantage of member priviledges to enhance your online shopping experience with us. If you have <small><strong>ANY</strong></small> questions about the operation of this online shop, please email the <a href="' . tep_href_link('contact_us.php') . '">store owner</a>.<br /><br />A confirmation has been sent to the provided email address. If you have not received it within the hour, please <a href="' . tep_href_link('contact_us.php') . '">contact us</a>.');
+
+// BOF POINTS REWARDS BS
+define('TEXT_WELCOME_POINTS_TITLE', 'As part of our Welcome to new customers, we have credited your account <u>%s</u> with a total of %s Shopping Points, worth %s .');
+define('TEXT_WELCOME_POINTS_LINK', 'Please refer to the <u>%s</u> as conditions may apply.');
+// EOF POINTS REWARDS BS
+?>
