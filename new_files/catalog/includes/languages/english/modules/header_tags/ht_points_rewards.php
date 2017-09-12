5c741a0d8b00f6ffdd9c5e788e4002501874c138
raiwa <info@sarplataygemas.com>
2017-09-12 18:44:35 +0200

    moved points check from checkout_confirmation.php to ht module

--- new_files/catalog/includes/languages/english/modules/header_tags/ht_points_rewards.php
+++ new_files/catalog/includes/languages/english/modules/header_tags/ht_points_rewards.php
@@ -12,4 +12,10 @@
 
   define('MODULE_HEADER_TAGS_POINTS_REWARDS_TITLE', 'Points and Rewards Module');
   define('MODULE_HEADER_TAGS_POINTS_REWARDS_DESCRIPTION', 'Use Points and Rewards Module');
+
+// used in checkout_confirmation.php
+  define('MODULE_HEADER_TAGS_POINTS_REWARDS_ERROR_POINTS_NOT', 'Points value are not enough to cover the cost of your purchase. Please select another payment method');
+  define('MODULE_HEADER_TAGS_POINTS_REWARDS_ERROR_NOT_VALID', 'The referral email does not appear to be valid - please make any necessary corrections.');
+  define('MODULE_HEADER_TAGS_POINTS_REWARDS_ERROR_NOT_FOUND', 'The referral email address you entered was not found.');
+  define('MODULE_HEADER_TAGS_POINTS_REWARDS_ERROR_SELF', 'Sorry you can not refer yourself.');
 ?>
--- new_files/catalog/includes/modules/header_tags/ht_points_rewards.php
+++ new_files/catalog/includes/modules/header_tags/ht_points_rewards.php
@@ -29,9 +29,53 @@
     }
 
     function execute() {
-      global $PHP_SELF, $oscTemplate, $customer_id, $product_info;
+      global $PHP_SELF, $oscTemplate, $customer_id, $product_info, $order, $customer_id, $payment, $$payment, $messageStack;
 
       switch (basename($PHP_SELF) ) {
+        case 'checkout_confirmation.php':
+          if ((MODULE_HEADER_TAGS_POINTS_REWARDS_USE_POINTS_SYSTEM == 'True') && (MODULE_HEADER_TAGS_POINTS_REWARDS_USE_REDEEM_SYSTEM == 'True')) {
+            if (isset($_POST['customer_shopping_points_spending']) && is_numeric($_POST['customer_shopping_points_spending']) && ($_POST['customer_shopping_points_spending'] > 0)) {
+              $customer_shopping_points_spending = false;
+              // This if sentence should include check for amount of points on account compared to the transferred point from checkout_payment.php
+              // Possible Hack Fix included
+              if (tep_calc_shopping_pvalue($_POST['customer_shopping_points_spending']) < $order->info['total'] && !is_object($$payment) || (tep_get_shopping_points($customer_id) < $_POST['customer_shopping_points_spending'])) {
+                $customer_shopping_points_spending = false;
+                $messageStack->add_session('header', MODULE_HEADER_TAGS_POINTS_REWARDS_ERROR_POINTS_NOT);
+                tep_redirect(tep_href_link('checkout_payment.php', '', 'SSL'));
+              } else {
+                $customer_shopping_points_spending = $_POST['customer_shopping_points_spending'];
+                if (!tep_session_is_registered('customer_shopping_points_spending')) tep_session_register('customer_shopping_points_spending');
+              }
+            }
+	  
+            //To ensure only the first order of a new customer is entitled to grant point to his/her referrer. Otherwise, a hacker might hard-code the email address of  a referrer and cheat for point on every single order the new customer made.
+            if (tep_not_null(MODULE_HEADER_TAGS_POINTS_REWARDS_POINTS_USE_REFERRAL_SYSTEM) && (tep_count_customer_orders() == 0)) {
+              if (isset($_POST['customer_referred']) && tep_not_null($_POST['customer_referred'])) {
+                $customer_referral = false;
+                $check_mail = trim($_POST['customer_referred']);
+                if (tep_validate_email($check_mail) == false) {
+                  $messageStack->add_session('header', MODULE_HEADER_TAGS_POINTS_REWARDS_ERROR_NOT_VALID);
+                  tep_redirect(tep_href_link('checkout_payment.php', '', 'SSL'));
+                } else {
+                  $valid_referral_query = tep_db_query("select customers_id from customers where customers_email_address = '" . $check_mail . "' limit 1");
+                  $valid_referral = tep_db_fetch_array($valid_referral_query);
+                  if (!tep_db_num_rows($valid_referral_query)) {
+                    $messageStack->add_session('header', MODULE_HEADER_TAGS_POINTS_REWARDS_ERROR_NOT_FOUND);
+                    tep_redirect(tep_href_link('checkout_payment.php', '', 'SSL'));
+                  }
+				  
+                  if ($check_mail == $order->customer['email_address']) {
+                    $messageStack->add_session('header', MODULE_HEADER_TAGS_POINTS_REWARDS_ERROR_SELF);
+                    tep_redirect(tep_href_link('checkout_payment.php', '', 'SSL'));
+                  } else {
+                    $customer_referral = $valid_referral['customers_id'];
+                    if (!tep_session_is_registered('customer_referral')) tep_session_register('customer_referral');
+                  }
+                }
+              }
+            }
+          }
+          break;
         case 'logoff.php':
           if (tep_session_is_registered('customer_shopping_points')) tep_session_unregister('customer_shopping_points');
           if (tep_session_is_registered('customer_shopping_points_spending')) tep_session_unregister('customer_shopping_points_spending');
