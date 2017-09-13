59957df56db9632a8aa4b811dc5de22c62a8d4bc
Lambros <flyingtomahawk2@gmail.com>
2017-09-13 16:18:47 +0900

    New shopping cart with points boxes module

---
+++ new_files/catalog/includes/languages/english/modules/boxes/bm_shopping_cart_points.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$ bm_shopping_cart_points.php
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2017 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_BOXES_SHOPPING_CART_POINTS_TITLE', 'Shopping Cart with Points');
+  define('MODULE_BOXES_SHOPPING_CART_POINTS_DESCRIPTION', 'Show shopping cart with points contents');
+  define('MODULE_BOXES_SHOPPING_CART_POINTS_BOX_TITLE', 'Shopping Cart');
+  define('MODULE_BOXES_SHOPPING_CART_POINTS_BOX_CART_EMPTY', '0 items');
+?>
