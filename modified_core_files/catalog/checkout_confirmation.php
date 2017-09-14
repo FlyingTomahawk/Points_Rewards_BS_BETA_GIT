acd134270a47e4cc5cb95b2d9525d4020cdc1152
raiwa <info@sarplataygemas.com>
2017-09-14 14:03:38 +0200

    fix for bug 2 on checkout_confirmation, revert move to ht

--- modified_core_files/catalog/checkout_confirmation.php
+++ modified_core_files/catalog/checkout_confirmation.php
@@ -43,6 +43,7 @@
     $comments = tep_db_prepare_input($_POST['comments']);
   }
 
+
 // load the selected payment module
   require('includes/classes/payment.php');
   $payment_modules = new payment($payment);
@@ -53,6 +54,10 @@
   $payment_modules->update_status();
 
 #####  BOF POINTS REWARDS BS  #######
+  if (!tep_session_is_registered('customer_shopping_points_spending')) tep_session_register('customer_shopping_points_spending');
+  if (isset($_POST['customer_shopping_points_spending']) && tep_not_null($_POST['customer_shopping_points_spending'])) {
+    $customer_shopping_points_spending = tep_db_prepare_input($_POST['customer_shopping_points_spending']);
+  }
   if ( ($payment_modules->selected_module != $payment) || ( is_array($payment_modules->modules) && (sizeof($payment_modules->modules) > 1) && !is_object($$payment) ) && (!$customer_shopping_points_spending) || (is_object($$payment) && ($$payment->enabled == false)) ) {
     tep_redirect(tep_href_link('checkout_payment.php', 'error_message=' . urlencode(ERROR_NO_PAYMENT_MODULE_SELECTED), 'SSL'));
   }
