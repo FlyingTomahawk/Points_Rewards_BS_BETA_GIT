c151085dc8c587d63f7c6bc8daa48107539416ca
raiwa <info@sarplataygemas.com>
2017-09-17 13:24:16 +0200

    move help link text constant from english.php to module

--- new_files/catalog/includes/languages/english/modules/content/reviews_points/cm_r_points.php
+++ new_files/catalog/includes/languages/english/modules/content/reviews_points/cm_r_points.php
@@ -14,3 +14,4 @@
   define('MODULE_CONTENT_REVIEWS_POINTS_DESCRIPTION', 'Show the product points block on the product info page.');
 
   define('MODULE_CONTENT_REVIEWS_POINTS_TEXT', 'Buy this item and earn %s points valued at %s.');
+  define('MODULE_CONTENT_REVIEWS_POINTS_HELP_LINK', ' Write a Review and earn <b>%s</b> worth of points.<br />Please check the %s for more information.');
--- new_files/catalog/includes/modules/content/reviews_points/templates/points.php
+++ new_files/catalog/includes/modules/content/reviews_points/templates/points.php
@@ -13,6 +13,6 @@
 ?>
 <!-- Start cm_r_points module -->
 <div id="cm_r_points" class="col-sm-<?php echo $content_width; ?>">
-  <?php echo '<p>' . sprintf(REVIEW_HELP_LINK, $currencies->format(tep_calc_shopping_pvalue(MODULE_HEADER_TAGS_POINTS_REWARDS_POINTS_USE_POINTS_FOR_REVIEWS)), '<a href="' . tep_href_link('my_points_help.php', '#heading13', $request_type) . '" title="' . BOX_INFORMATION_MY_POINTS_HELP . '">' . BOX_INFORMATION_MY_POINTS_HELP . '</a>') . '</p>'; ?>
+  <?php echo '<p>' . sprintf(MODULE_CONTENT_REVIEWS_POINTS_HELP_LINK, $currencies->format(tep_calc_shopping_pvalue(MODULE_HEADER_TAGS_POINTS_REWARDS_POINTS_USE_POINTS_FOR_REVIEWS)), '<a href="' . tep_href_link('my_points_help.php', '#heading13', $request_type) . '" title="' . BOX_INFORMATION_MY_POINTS_HELP . '">' . BOX_INFORMATION_MY_POINTS_HELP . '</a>') . '</p>'; ?>
 </div>
 <!-- End cm_r_points module -->
