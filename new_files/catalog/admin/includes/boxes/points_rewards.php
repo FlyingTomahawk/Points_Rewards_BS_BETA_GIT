49bcae3957621e9915bea886fe5a7ca8fbcdf11c
raiwa <info@sarplataygemas.com>
2017-09-19 18:56:38 +0200

    move language definitions for admin box to box language file

--- new_files/catalog/admin/includes/boxes/points_rewards.php
+++ new_files/catalog/admin/includes/boxes/points_rewards.php
@@ -16,15 +16,15 @@
     foreach ( $cl_box_groups as &$group ) {
     if ( $group['heading'] == BOX_HEADING_CUSTOMERS ) {
       $group['apps'][] = array('code' => 'customers_points.php',
-							   'title' => BOX_CUSTOMERS_POINTS,
+							   'title' => MODULES_ADMIN_MENUCUSTOMERS_POINTS,
 							   'link' => tep_href_link('customers_points.php')
 							   );
 	  $group['apps'][] = array('code' => 'customers_points_pending.php',
-							   'title' => BOX_CUSTOMERS_POINTS_PENDING,
+							   'title' => MODULES_ADMIN_MENUCUSTOMERS_POINTS_PENDING,
 							   'link' => tep_href_link('customers_points_pending.php')
 							   );
 	  $group['apps'][] = array('code' => 'customers_points_referral.php',
-							   'title' => BOX_CUSTOMERS_POINTS_REFERRAL,
+							   'title' => MODULES_ADMIN_MENUCUSTOMERS_POINTS_REFERRAL,
 							   'link' => tep_href_link('customers_points_referral.php')	  
 						       );
       break;
---
+++ new_files/catalog/admin/includes/languages/english/modules/boxes/points_rewards.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULES_ADMIN_MENUCUSTOMERS_POINTS', 'Customers Points');
+define('MODULES_ADMIN_MENUCUSTOMERS_POINTS_PENDING', 'Pending Points');
+define('MODULES_ADMIN_MENUCUSTOMERS_POINTS_REFERRAL', 'Referral Points');
