fffc8b1a7487b6909ef7f06431b9112df159f3e6
Lambros <flyingtomahawk2@gmail.com>
2017-10-10 19:51:35 +0900

    Added German translation for modified core files

---
+++ modified_core_files/catalog/includes/languages/german/modules/content/footer/cm_footer_information_links.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_FOOTER_INFORMATION_TITLE', 'Informationen Links Block');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_DESCRIPTION', 'Fügt Informationslinks in den Footer-Bereich Ihrer Website ein');
+
+  define('MODULE_CONTENT_FOOTER_INFORMATION_HEADING_TITLE', 'Information');
+
+  define('MODULE_CONTENT_FOOTER_INFORMATION_SHIPPING', 'Liefer- und Versandkosten');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_PRIVACY', 'Datenschutzerklärung');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_CONDITIONS', 'AGB');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_CONTACT', 'Kontakt');
+  define('MODULE_CONTENT_FOOTER_INFORMATION_MY_POINTS_HELP', 'Bonuspunkte FAQ');
+
